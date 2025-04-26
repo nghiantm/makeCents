34f850943ab3429c53bc5c68225ac5bf69ac87a0
Matt Nguyen <nguyentrongminhnghia@gmail.com>
2025-04-26 15:59:42 -0400

    added some components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
-import Home from './assets/routes/Home'
+import Home from './routes/Home.jsx'
+import NavBar from './components/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='h-screen'>
+    <div className='h-screen w-screen p-4'>
+      <NavBar />
       <Routes>
         <Route  path='/' element={<Home />} />
       </Routes>
--- frontend/src/routes/Home.jsx
+++ frontend/src/routes/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from '../../redux/counterSlice';
+import { increment, decrement } from '../redux/counterSlice';  
 
 const Home = () => {
     const count = useSelector((state) => state.counter.value);
